bed8e8baf8925dc455cbfdccebbc2ad550f30df3
alvinwong12 <alvinwong312@gmail.com>
2019-01-18 01:02:42 -0500

    added product and cart routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products
+  
+  get "/carts", to: "carts#index"
+  get "/carts/:id", to: "carts#show"
+  post "/carts", to: "carts#create"
+  post "/carts/:id/products", to: "carts#add"
+  post "/carts/:id/activate", to: "carts#activate"
+  
 end
