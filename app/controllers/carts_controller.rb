fc433fa5f3d99311a03e3c02ee96e581e96dfdce
alvinwong12 <alvinwong312@gmail.com>
2019-01-19 03:22:29 -0500

    format output

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,31 +2,30 @@ class CartsController < ApplicationController
 
   def index
     carts = Cart.all
-    render json: carts.to_json(include: :cart_items), status: 200
+    render json: carts.to_json(include: [:cart_items => {only: :quantity, include: [:product => {except: :id}]}], except: :id), status: 200
   end
 
   def show
     cart = Cart.find_by_cart_id(cart_id)
-    render json: cart.to_json(include: :cart_items), status: 200
+    render json: cart.to_json(include: [:cart_items => {only: :quantity, include: [:product => {except: :id}]}], except: :id), status: 200
   end
 
   def create
     cart = Cart.new
     cart.save
-    render json: cart.to_json(include: :cart_items), status: 200
+    render json: cart.to_json(include: [:cart_items => {only: :quantity, include: [:product => {except: :id}]}], except: :id), status: 201
   end
 
   def add
-    # cannot add product if purchase is compledted
-    cart = Cart.find_by_cart_id(cart_id).first
+    cart = Cart.find_by_cart_id(cart_id)
     cart.add_products(products)
-    render json: cart.to_json(include: :cart_items), status: 200 
+    render json: cart.to_json(include: [:cart_items => {only: :quantity, include: [:product => {except: :id}]}], except: :id), status: 200 
   end
 
   def activate
-    cart = Cart.find_by_cart_id(cart_id).first
-    cart.activate unless cart.nil?
-    render json: cart.to_json(include: :cart_items), status: 200
+    cart = Cart.find_by_cart_id(cart_id)
+    cart.activate
+    render json: cart.to_json(include: [:cart_items => {only: :quantity, include: [:product => {except: :id}]}], except: :id), status: 200
   end
 
   private
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,18 +1,13 @@
 class ProductsController < ApplicationController
 
   def index
-    if query_available_products?
-      products = Product.find_by_params(product_params).available_products.limit(amount_to_query)
-    else
-      products = Product.find_by_params(product_params).limit(amount_to_query)
-    end
-
-    render json: products.to_json(:except => [:id]), status: 200
+    products = Product.find_products(product_params, query_available_products?, amount_to_query)
+    render json: products.to_json(:except => [:id]) , status: 200
   end
 
   def show
     product = Product.find_by_product_id(params[:id])
-    render json: product.to_json, status: 200
+    render json: product.to_json(except: :id), status: 200
   end
 
   private
