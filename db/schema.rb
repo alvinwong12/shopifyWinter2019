4440552c73f65fe3137259dccee89a39931b8253
alvinwong12 <alvinwong312@gmail.com>
2019-01-19 06:26:32 -0500

    seed default products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_18_012748) do
+ActiveRecord::Schema.define(version: 2019_01_17_211656) do
 
   create_table "cart_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.decimal "quantity", precision: 10, default: "0"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(title: "tequila", price: 10.65, inventory_count: 100)
+Product.create(title: "vodka", price: 8.92, inventory_count: 46)
+Product.create(title: "whiskey", price: 25.99, inventory_count: 21)
+Product.create(title: "rum", price: 12.15, inventory_count: 79)
+Product.create(title: "soju", price: 9.01, inventory_count: 5)
+Product.create(title: "beer", price: 3.52, inventory_count: 37)
+Product.create(title: "jagermeister", price: 20.2, inventory_count: 0)
