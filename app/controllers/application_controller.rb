8c3c48ec1f44ec690e225cc8e58ff17a46c58c14
alvinwong12 <alvinwong312@gmail.com>
2019-01-19 03:22:14 -0500

    handle custom exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+  rescue_from ApiExceptions::CartError::ActivationFail, with: :render_error_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+  def render_error_response(err)
+    render json: err_json(err), status: 200
+  end
+
+  def render_not_found(err)
+    render json: err_json(err), status: 404
+  end
+
+  private
+
+  def err_json(err)
+    {
+      status: "error",
+      message: err
+    }
+  end
+
 end
